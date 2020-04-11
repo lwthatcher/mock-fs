d5241f23c3f3b9cf13cd44036d9a3117fc1915cb
Lawrence <lwthatcher@msn.com>
2020-04-11 15:54:25 -0700

    split_path() can include name arg

--- mockfs/util.py
+++ mockfs/util.py
@@ -7,8 +7,17 @@ class IllegalFileSystemOperationError(Exception):
   pass
 
 
-def split_path(path):
-  """splits the provided path elements into a tuple"""
-  if isinstance(path, str):
+def split_path(path, name=None):
+  """Splits the provided path elements into a tuple.
+  
+  This method also handles the cases where path is already a tuple or a list.
+  If name is provided, it will be appended to the end of the tuple.
+  """
+  # split apart path if non-empty string
+  if path and isinstance(path, str):
     path = path.split('\\')
+  # append name if present
+  if name is not None:
+    path = list(path) + [name]
+  # return as a tuple
   return tuple(path)
--- test/test_util.py
+++ test/test_util.py
@@ -17,3 +17,27 @@ class TestUtil(TestCase):
     # list
     path = split_path(['D', 'dir', 'subdir', 'zip'])
     self.assertEqual(path, ('D', 'dir', 'subdir', 'zip'))
+
+  def test_split_path__name_included(self):
+    # single-item string + name
+    path = split_path('D', 'dir')
+    self.assertEqual(path, ('D', 'dir'))
+    # multi-item string + name
+    path = split_path('D\\dir\\subdir\\zip', 'file.txt')
+    self.assertEqual(path, ('D', 'dir', 'subdir', 'zip', 'file.txt'))
+    # tuple + name
+    path = split_path(('D', 'dir', 'subdir', 'zip'), 'file.txt')
+    self.assertEqual(path, ('D', 'dir', 'subdir', 'zip', 'file.txt'))
+    # list + name
+    path = split_path(['D', 'dir', 'subdir', 'zip'], 'file.txt')
+    self.assertEqual(path, ('D', 'dir', 'subdir', 'zip', 'file.txt'))
+
+  def test_split_path__empty_path__name_included(self):
+    path = split_path('', 'name')
+    self.assertEqual(path, ('name',))
+
+  def test_split_path__empty_name(self):
+    # probably not desirable, but currently the expected behavior
+    path = split_path('D', '')
+    self.assertEqual(path, ('D', ''))
+
