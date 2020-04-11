bbea5aef206cc17fd42a2e01c3053bce11246c18
Lawrence <lwthatcher@msn.com>
2020-04-11 14:16:16 -0700

    added Name property tests

--- test/test_entities.py
+++ test/test_entities.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from mockfs import MockFileSystem
 from mockfs.entities import FolderEntity, DriveEntity, TextFileEntity, ZipFileEntity
 
+# region [Folder Tests]
 class TestFolderEntity(TestCase):
 
   def test_constructor(self):
@@ -14,7 +15,13 @@ class TestFolderEntity(TestCase):
     f = FolderEntity(fs, ('D', 'dir'))
     self.assertEqual(f.Type, 'folder')
 
+  def test_Name(self):
+    fs = MockFileSystem()
+    f = FolderEntity(fs, ('D', 'dir'))
+    self.assertEqual(f.Name, 'dir')
+# endregion
 
+# region [Drive Tests]
 class TestDriveEntity(TestCase):
 
   def test_constructor(self):
@@ -27,7 +34,14 @@ class TestDriveEntity(TestCase):
     f = DriveEntity(fs, ('D'))
     self.assertEqual(f.Type, 'drive')
 
+  def test_Name(self):
+    fs = MockFileSystem()
+    f = DriveEntity(fs, ('D'))
+    self.assertEqual(f.Name, 'D')
+# endregion
 
+
+# region [Text File Tests]
 class TestTextFileEntity(TestCase):
 
   def test_constructor(self):
@@ -39,8 +53,15 @@ class TestTextFileEntity(TestCase):
     fs = MockFileSystem()
     f = TextFileEntity(fs, ('D', 'file.txt'))
     self.assertEqual(f.Type, 'text')
+  
+  def test_Name(self):
+    fs = MockFileSystem()
+    f = TextFileEntity(fs, ('D', 'file.txt'))
+    self.assertEqual(f.Name, 'file.txt')
+# endregion
 
 
+# region [Zip File Tests]
 class TestZipFileEntity(TestCase):
 
   def test_constructor(self):
@@ -52,3 +73,9 @@ class TestZipFileEntity(TestCase):
     fs = MockFileSystem()
     f = ZipFileEntity(fs, ('D', 'zippy.zip'))
     self.assertEqual(f.Type, 'zip')
+
+  def test_Name(self):
+    fs = MockFileSystem()
+    f = ZipFileEntity(fs, ('D', 'zippy.zip'))
+    self.assertEqual(f.Name, 'zippy.zip')
+# endregion
